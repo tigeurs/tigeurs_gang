fe01f268e7ad73cce0bc4d88e1e0145254354001
tigeurs <69429871+tigeurs@users.noreply.github.com>
2022-03-24 23:23:38 +0100

    Update esx_gang.sql

--- esx_gang.sql
+++ esx_gang.sql
@@ -10,7 +10,7 @@ INSERT INTO `datastore` (name, label, shared) VALUES
   ("society_vagos", "Vagos", 1),
   ("society_ballas", "Ballas", 1);
 
-INSERT INTO `job2_grades` (job2_name, grade, name, label, salary, skin_male, skin_female) VALUES
+INSERT INTO `job_grades` (job2_name, grade, name, label, salary, skin_male, skin_female) VALUES
     ("vagos", 0, "pequeno", "Pequeño", 0, "{}", "{}"),
     ("vagos", 1, "asistente", "Asistente", 0, "{}", "{}"),
     ("vagos", 2, "teniente", "Teniente", 0, "{}", "{}"),
@@ -22,6 +22,6 @@ INSERT INTO `job2_grades` (job2_name, grade, name, label, salary, skin_male, ski
     ("ballas", 2, "bras", "Bras Droit", 0, "{}", "{}"),
     ("ballas", 3, "boss", "OG", 0, "{}", "{}");
 
-INSERT INTO `jobs2` (name, label) VALUES
+INSERT INTO `jobs` (name, label) VALUES
   ("vagos", "Vagos"),
   ("ballas", "Ballas");
